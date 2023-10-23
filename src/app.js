07d2616db40f68710193d56d24a906d8184512de
Daniel Pérez Arenas <144001482+Danielperezarenas@users.noreply.github.com>
2023-10-23 00:37:05 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,40 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  
+  let cardsType = ['♦', '♥', '♠', '♣']
+  let cards = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"]
+
+  function getTypeOfCard() {
+    let typeIndex = Math.floor(Math.random() * cardsType.length);
+    let randomType = cardsType[typeIndex];
+    
+    let typeOfCard = document.querySelectorAll(".suits");
+    typeOfCard.forEach(typeOfCardElement => {
+      typeOfCardElement.innerHTML = randomType;
+      
+      if (randomType == '♦' || randomType == '♥') {
+        typeOfCardElement.style.color = ("red")
+      } else {
+        typeOfCardElement.style.color = ("black")
+      }
+    });
+  }
+  getTypeOfCard();
+  
+  function getCard() {
+    let cardsIndex = Math.floor(Math.random() * cards.length);
+    let randomCard = cards[cardsIndex];
+    
+    let card = document.querySelector(".card-body");
+    card.innerHTML = randomCard;
+  }
+  getCard();
+
+  function getRandomCard(){
+    getTypeOfCard();
+    getCard();
+  }
+
+  document.querySelector(".btn").addEventListener("click", getRandomCard);
+}
